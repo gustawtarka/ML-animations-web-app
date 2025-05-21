339c9b5eb6f8bcaa5c1f943768353ea436423f0a
VyrTtZ <tarkagustaw@gmail.com>
2025-05-21 23:36:19 +0100

    fixed one comment

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -5,25 +5,6 @@ import { v4 as uuidv4 } from 'uuid';
 import accounts from './accounts.js';
 import dataStore from '../models/data-store.js'
 //--------------------------------------------- Creates object dashboard
-
-/*
-createView(request, response) {
-    logger.info('dashboard rendering');
-    const loggedInUser = accounts.getCurrentUser(request);
-    if (loggedInUser) {
-            console.log(loggedInUser.id);
-    const viewData = {
-      title: 'Neutron Star database dashboard',
-      starCollection: starStore.getUserStars(loggedInUser.id),
-      fullname: loggedInUser.firstName + ' ' + loggedInUser.lastName,
-      pfp: loggedInUser.picture,
-      timespent: 0,
-    };
-    logger.info('about to render' + viewData.starCollection);
-    response.render('dashboard', viewData);
-    }
-    else response.redirect('/');
-    */
 const dashboard = {
     createView(request, response) {
       const loggedInUser = accounts.getCurrentUser(request);
